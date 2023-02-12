a830f63f9888e106604c903c2ccc77a83b6925db
Spiguerfild <luique16@outlook.com>
2023-02-12 14:00:10 -0400

    type writing funcionando

--- src/components/Apresentacao.jsx
+++ src/components/Apresentacao.jsx
@@ -6,9 +6,9 @@ function Apresentacao() {
       <div className="container-apresentacao">
         <div className="text-ap">
           <h1>Hi, i'm Luis Henrique</h1>
-          <div className="type-anim">
-            <p>Front-end Developer</p>
-          </div>
+        
+            <p class="line-1 anim-typewriter">Front-end Developer </p>
+       
         </div>
 
         <div className="div-foto-ap">
