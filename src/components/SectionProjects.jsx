d41b571e16f76caa951556466ac7a7e388bd4976
Luis Henrique <luique16@outlook.com>
2023-02-14 17:51:11 -0400

    ✨ cards ok

--- src/components/SectionProjects.jsx
+++ src/components/SectionProjects.jsx
@@ -3,7 +3,8 @@ import TopSections from "./TopSections.jsx";
 import img_1 from "../img/projetos_cadastro_basico.png";
 import img_2 from "../img/projetos_pokedex.png";
 import img_3 from "../img/projetos_buscar_cep.png";
-import git_img from "../img/"
+import git_img from "../img/github.png";
+import lin_img from "../img/linkedin.png";
 function SectionProjects() {
   return (
     <div id="section-projects">
@@ -11,16 +12,42 @@ function SectionProjects() {
       <div className="container-projects">
         <div className="card-project">
           <img src={img_1} alt="" width={254} />
+
+          <a
+            href="https://github.com/Spiguerfild/projeto_1_danki"
+            target="_blank"
+          >
+            <img className="icon-prj" src={git_img} alt="" /> GitLink
+          </a>
+          <a
+            href="https://www.linkedin.com/posts/luis-henrique-spiga_quase-finalizando-a-cria%C3%A7%C3%A3o-da-p%C3%A1gina-ainda-activity-6968580660055736320-IXne?utm_source=share&utm_medium=member_desktop"
+            target="_blank"
+          >
+            <img className="icon-prj" src={lin_img} /> Linkedin
+          </a>
           <h3>Simple Page</h3>
         </div>
         <div className="card-project">
           <img src={img_2} alt="" width={254} />
-          <a href="https://github.com/Spiguerfild/Pokedex"><img src={}/> GitLink</a>
-          <a href="https://github.com/Spiguerfild/Pokedex">GitLink</a>
+          <a href="https://github.com/Spiguerfild/Pokedex" target="_blank">
+            <img className="icon-prj" src={git_img} alt="" /> GitLink
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/posts/luis-henrique-spiga_mais-um-projeto-realizado-com-sucesso-activity-7020794796826869760-e3wo/?utm_source=share&utm_medium=member_desktop"
+          >
+            <img className="icon-prj" src={lin_img} /> Linkedin
+          </a>
           <h3>Pokedex</h3>
         </div>
         <div className="card-project">
           <img src={img_3} alt="" width={254} />
+          <a href="https://github.com/Spiguerfild/buscador-CEP-REACT" target="_blank">
+            <img className="icon-prj" src={git_img} alt="" /> GitLink
+          </a>
+          <a href="https://www.linkedin.com/posts/luis-henrique-spiga_projeto-pesquisador-de-cep-utilizei-s%C3%B3-activity-7023872375658037248-yowQ?utm_source=share&utm_medium=member_desktop">
+            <img className="icon-prj" src={lin_img} /> Linkedin
+          </a>
           <h3>Busca CEP</h3>
         </div>
       </div>
