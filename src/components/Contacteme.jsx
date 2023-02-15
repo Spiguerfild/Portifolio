103b8f5d28e1a1b563c652ee1fe77303143c3c6a
Spiguerfild <luique16@outlook.com>
2023-02-14 21:04:34 -0400

    save

--- src/components/Contacteme.jsx
+++ src/components/Contacteme.jsx
@@ -10,8 +10,11 @@ function Contactme() {
             <div className='container-contact'>
                 <div className='chave'><h1>{txt1}</h1></div>
                 <div className='center'>
-                    <h2 className='infos'><h3 className='content'></h3></h2>
-                    <h2 className='infos'>Whatsapp<h3 className='content'>https://api.whatsapp.com/send/?phone=44984342957&text&type=phone_number&app_absent=0</h3></h2>
+
+                    <div className='infos'>
+                        <img src="" alt="" />
+                        <a href='https://api.whatsapp.com/send/?phone=44984342957&text&type=phone_number&app_absent=0' target="_blank" className='content'>Send me a message</a>
+                    </div>
 
                 </div>
                 <div className='chave'><h1>{txt2}</h1></div>
