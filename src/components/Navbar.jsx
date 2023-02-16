d27055314edfa5c77fb7a3197604a9c7504048f3
Spiguerfild <luique16@outlook.com>
2023-02-16 13:56:44 -0400

    test_responsevity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,36 +3,52 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div id="navbar">
-      <ul className="navbar-portifolio">
-        <div className="title">
-          <h1>
-            Lui<span>s Hen</span>rique
-          </h1>
-        </div>
+      <div class="container">
 
-        <div className="links">
-          <li>
-            <a className="link-slide" href="#apresentacao">
-              Home
-            </a>
-          </li>
-          <li>
-            <a className="link-slide" href="#Section-history-container">
-              History
-            </a>
-          </li>
-          <li>
-            <a className="link-slide" href="#section-projects">
-              Projects
-            </a>
-          </li>
-          <li>
-            <a className="link-slide" href="#contact-me">
-              Contactme
-            </a>
-          </li>
-        </div>
-      </ul>
+
+        <ul className="navbar-portifolio">
+          <div class="row">
+            <div className="col-3">
+              <div className="title col-lg-3" >
+                <h1>
+                  Lui<span>s Hen</span>rique
+                </h1>
+              </div>
+            </div>
+            <div className="col-9">
+
+              <div className="links">
+
+                <li className="col-2">
+                  <a className="link-slide" href="#apresentacao">
+                    Home
+                  </a>
+                </li>
+
+                <div className="col-2">
+                  <li className="">
+                    <a className="link-slide" href="#Section-history-container">
+                      History
+                    </a>
+                  </li>
+                </div>
+
+                <li className="col-2">
+                  <a className="link-slide" href="#section-projects">
+                    Projects
+                  </a>
+                </li>
+                <li className="col-2" >
+                  <a className="link-slide" href="#contact-me">
+                    Contactme
+                  </a>
+                </li>
+              </div>
+            </div>
+          </div>
+        </ul>
+      </div>
+
     </div>
   );
 }
